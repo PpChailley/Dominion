3d6133f052e1a1014d90252d41987bfacefde10a
gbd <gbd@gmx.com>
2015-06-05 11:07:52 -0400

    IMPORT perso libs (not formalizeed into libs yet)

---
+++ Dominion/Tools/ProcessExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace org.gbd.Dominion.Tools
+{
+  public static class ProcessExtensions
+  {
+    [DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
+    public static extern IntPtr SetFocus(HandleRef hWnd);
+
+
+    public static void SetFocus(this Process p)
+    {
+      IntPtr hWnd = p.Handle;
+      SetFocus(new HandleRef(null, hWnd));
+
+    }
+  }
+}
---
+++ Dominion/Tools/SystemIOExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+
+namespace org.gbd.Dominion.Tools
+{
+  public static class SystemIoExtensions
+  {
+    /// <summary>
+    ///   Recursively create directory
+    /// </summary>
+    /// <param name="dirInfo"> Folder path to create. </param>
+    public static void CreateRecursive(this DirectoryInfo dirInfo)
+    {
+      if (dirInfo.Parent != null) CreateRecursive(dirInfo.Parent);
+      if (!dirInfo.Exists) dirInfo.Create();
+    }
+  }
+}
