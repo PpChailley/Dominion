f19ffd1efa4ec140da1ef37d3f929726f1fd28f4
gbd <gbd@gmx.com>
2015-06-05 17:23:41 -0400

    REFACTOR before feature supplyZone

---
+++ Dominion/Model/Actions/GameActionTargetingPlayers.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace org.gbd.Dominion.Model.Actions
+{
+    public abstract class GameActionTargetingPlayers : IGameActionTargetingPlayers
+    {
+
+        public ICollection<PlayerChoice> DesignatedPlayers;
+
+
+        public void Do()
+        {
+            foreach (var player in Player.Get(DesignatedPlayers))
+            {
+                this.DoToPlayer(player);
+            }
+        }
+
+        protected abstract void DoToPlayer(Player p);
+
+    }
+}
--- Dominion/Model/Actions/IGameActionTargetingPlayers.cs
+++ Dominion/Model/Actions/IGameActionTargetingPlayers.cs
@@ -1,26 +1,6 @@
-﻿using System.Collections.Generic;
-
-namespace org.gbd.Dominion.Model.Actions
+﻿namespace org.gbd.Dominion.Model.Actions
 {
     public interface IGameActionTargetingPlayers: IGameAction
     {
-    }
-
-    public abstract class GameActionTargetingPlayers : IGameActionTargetingPlayers
-    {
-
-        public ICollection<PlayerChoice> DesignatedPlayers;
-
-
-        public void Do()
-        {
-            foreach (var player in Player.Get(DesignatedPlayers))
-            {
-                this.DoToPlayer(player);
-            }
-        }
-
-        protected abstract void DoToPlayer(Player p);
-
     }
 }
