8d9bd6a1611edf7fd6db13d713b29d121c2a4633
gbd <gbd@gmx.com>
2015-06-15 20:36:52 -0400

    CLEANUP Remove old code

--- Dominion/Model/Zones/SupplyPile.cs
+++ Dominion/Model/Zones/SupplyPile.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using gbd.Dominion.Model.GameMechanics;
+﻿using System.Collections.Generic;
 using gbd.Dominion.Tools;
-using gbd.Tools.Cli;
-using gbd.Tools.NInject;
 using Ninject;
 
 namespace gbd.Dominion.Model.Zones
@@ -21,22 +16,5 @@ namespace gbd.Dominion.Model.Zones
             Cards = IoC.Kernel.Get<IList<ICard>>();
         }
 
-
-/*  
- * TODO: remove this (it should be done by NInject)
-        public SupplyPile()
-        {
-            Cards = new List<ICard>();
-
-            for (var i = 0; i < DEFAULT_SUPPLY_PILE_SIZE; i++)
-            {
-                Cards.Add(IoC.Kernel.Get<ICard>());
-            }
-        }
-
-  */      
-
-
- 
     }
 }
