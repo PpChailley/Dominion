d9cc1406ee5a5758d67e8acd322d7ebbbba1bca0
gbd <gbd@gmx.com>
2015-06-23 18:51:07 -0400

    MORE Add a Curse pile

---
+++ Dominion/Model/Zones/CursePile.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using gbd.Dominion.Model.Cards;
+
+namespace gbd.Dominion.Model.Zones
+{
+    public class CursePile: SupplyPile
+    {
+        public CursePile(IList<ICard> cards) : base(cards){}
+    }
+}
